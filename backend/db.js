d179df1ed0537a648e44fbabf4f969f090773a9c
cloudsangwon <lee2155507@naver.com>
2020-11-26 23:53:36 +0900

    aws.json file fixed

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 // const mysql = require('mysql');
 // const pool = mysql.createPool({
-//     host:'nodejs-rds.cbn49nh1i8lf.ap-northeast-2.rds.amazonaws.com',
+//     host:'',
 //     port:3306,
-//     user:'admin',
-//     password:'Tkddnjs1!',
-//     database:'sangwon_nodejs'
+//     user:'',
+//     password:'',
+//     database:''
 // });
 // exports.pool = pool;
 
